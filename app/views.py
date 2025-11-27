bfb3673609472b2bb9686e7a7d4a2358f78384ef
Antonio Gabriel <biepe07@gmail.com>
2025-11-27 20:43:01 -0300

    Configurado as urls do app e criado a view de listar as OS

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
+from django.views.generic import CreateView, UpdateView, ListView, DeleteView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import OrdemServico
+from .forms import OsForm
+
+class ListOS(ListView):
+    model = OrdemServico
+    context_object_name = 'os'
+    template_name = 'list_os.html'
+
