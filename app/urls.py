1ce00241d378fd92a71b5e1699baa2e98459ade8
Antonio Gabriel <biepe07@gmail.com>
2025-11-28 20:06:10 -0300

    Feito as views para logar e deslogar do sistema

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('listar/', views.ListOS.as_view(), name='listar'),
     path('criar/', views.CreateOS.as_view(), name='criar'),
     path('atualizar/<int:pk>/', views.UpdateOS.as_view(), name='atualizar'),
     path('detalhar/<int:pk>/', views.DetailOS.as_view(), name='detalhar'),
-    path('pdf/<int:pk>/', views.generate_pdf, name='baixar_pdf')
+    path('pdf/<int:pk>/', views.generate_pdf, name='baixar_pdf'),
+    path('login/', views.logar, name='logar'),
+    path('logout/', views.deslogar, name='deslogar'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,20 +1,26 @@
 from django.views.generic import CreateView, UpdateView, ListView, DeleteView, DetailView
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.template.loader import render_to_string
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.http import HttpResponse
-from django.shortcuts import render
 
-from .models import OrdemServico
+from .models import OrdemServico, User
 from .forms import OsForm
 
 from weasyprint import HTML
 
-class ListOS(ListView):
+class RequerLogin(LoginRequiredMixin):
+    login_url = '/login/'
+    redirect_field_name = 'redirect_to'
+
+class ListOS(RequerLogin, ListView):
     model = OrdemServico
     context_object_name = 'ordens'
     template_name = 'list_os.html'
 
-class CreateOS(CreateView):
+class CreateOS(RequerLogin, CreateView):
     model = OrdemServico
     form_class = OsForm
     context_object_name = 'ordens'    
@@ -25,16 +31,19 @@ class CreateOS(CreateView):
         form.instance.atendente_responsavel = self.request.user
         return super().form_valid(form)
 
-class DetailOS(DetailView):
+class DetailOS(RequerLogin, DetailView):
     model = OrdemServico
     template_name = 'os_pdf.html'
     context_object_name = 'os'
 
-class UpdateOS(UpdateView):
+class UpdateOS(RequerLogin, UpdateView):
     model = OrdemServico
     context_object_name = 'os'    
     template_name = 'os_pdf.html'
     success_url = reverse_lazy('listar')
+
+def home(request):
+    return render(request, 'home.html')
 
 def generate_pdf(request, pk):
     os = OrdemServico.objects.get(pk = pk)
@@ -46,3 +55,41 @@ def generate_pdf(request, pk):
     response["Content-Disposition"] = f"inline; filename={os}.pdf"
 
     return response
+
+def logar(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:
+        name = request.POST.get('nome')
+        password = request.POST.get('senha')
+
+        user = authenticate(request, username=name, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            return HttpResponse(render(request, 'usuario_nao_existe.html'))
+        
+
+def deslogar(request):
+    logout(request)
+    return redirect('logar')
+
+# def registrar(request):
+#     if request.method == "GET":
+#         return render(request, 'register.html')
+#     else:
+#         name = request.POST.get('nome')
+#         password = request.POST.get('senha')
+
+#         user = User.objects.filter(username = name, password = password)
+
+#         if user:
+#             return HttpResponse(render(request, 'usuario_ja_existe.html'))
+#         else:
+#             user = User.objects.create_user(username=name, password= password)
+#             user.save()
+
+#     return redirect('todo:logar')
+         
