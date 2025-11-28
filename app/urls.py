e6c822e9a521dd8a7f364ccb74c0cb414386007f
Antonio Gabriel <biepe07@gmail.com>
2025-11-27 21:58:48 -0300

    Crud completo

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('listar/', views.ListOS.as_view())
+    path('listar/', views.ListOS.as_view(), name='listar'),
+    path('criar/', views.CreateOS.as_view(), name='criar'),
+    path('atualizar/<int:pk>/', views.UpdateOS.as_view(), name='atualizar'),
+    path('detalhar/<int:pk>/', views.DetailOS.as_view(), name='detalhar')
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import CreateView, UpdateView, ListView, DeleteView
+from django.views.generic import CreateView, UpdateView, ListView, DeleteView, DetailView
+from django.urls import reverse_lazy
 from django.shortcuts import render
 
 from .models import OrdemServico
@@ -6,6 +7,27 @@ from .forms import OsForm
 
 class ListOS(ListView):
     model = OrdemServico
-    context_object_name = 'os'
+    context_object_name = 'ordens'
     template_name = 'list_os.html'
 
+class CreateOS(CreateView):
+    model = OrdemServico
+    form_class = OsForm
+    context_object_name = 'ordens'    
+    template_name = 'create_os.html'
+    success_url = reverse_lazy('listar')
+
+    def form_valid(self, form):
+        form.instance.atendente_responsavel = self.request.user
+        return super().form_valid(form)
+
+class DetailOS(DetailView):
+    model = OrdemServico
+    template_name = 'os_pdf.html'
+    context_object_name = 'os'
+
+class UpdateOS(UpdateView):
+    model = OrdemServico
+    context_object_name = 'os'    
+    template_name = 'os_pdf.html'
+    success_url = reverse_lazy('listar')
