ccfa92ef206ef5d5758cb76d13f1391a245da9eb
Antonio Gabriel <biepe07@gmail.com>
2025-12-02 17:13:36 -0300

    Adcionando staticfiles

--- setup/settings.py
+++ setup/settings.py
@@ -1,5 +1,4 @@
-
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -103,6 +102,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
